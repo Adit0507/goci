9fa25348af8e6e8770556673fad70d9c30a3aead
Adit0507 <adi05072002@gmail.com>
2024-12-15 12:39:49 +0530

    executing external programs

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"os"
+	"os/exec"
 )
+
+func main() {
+	proj := flag.String("p", "", "Project directory")
+	flag.Parse()
+
+	if err := run(*proj, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
 
 func run(proj string, out io.Writer) error{
 	// check to see if project direcory is proviided
@@ -11,5 +25,15 @@ func run(proj string, out io.Writer) error{
 		return fmt.Errorf("Project directory is required")
 	}
 
-	return nil
+	args := []string{"build", ".", "errors"}
+	cmd := exec.Command("go", args...)
+	cmd.Dir = proj
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("'go build' failed: %s", err)
+	}
+
+	_, err := fmt.Fprintln(out, "Go build: SUCESS!")
+
+	return err
 }
